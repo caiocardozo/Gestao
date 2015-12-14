8e6e60c1e0354322c6ae82d4d8246142e31bb546
Caio Cardozo <caiocardozocb3@gmail.com>
2015-12-14 19:26:07 -0200

    jjjjjjjjjjjj

--- GestaoDDD.Domain/Entities/Servico.cs
+++ GestaoDDD.Domain/Entities/Servico.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace GestaoDDD.Domain.Entities
 {
-    public class Servico: Entidade
+    public class Servico : Entidade
     {
         public int serv_Id { get; set; }
         public string serv_nome { get; set; }
