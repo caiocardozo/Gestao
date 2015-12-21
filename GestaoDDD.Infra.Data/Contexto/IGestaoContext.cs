6d95e89c95a5a3126e3ed4c347b3fa2a07f9d74d
Wagner Nogueira <wagnogueira19@hotmail.com>
2015-12-21 11:28:11 -0200

    ajuste no Contexto e na CSS da pagina de captura

--- GestaoDDD.Infra.Data/Contexto/IGestaoContext.cs
+++ GestaoDDD.Infra.Data/Contexto/IGestaoContext.cs
@@ -4,12 +4,17 @@ using GestaoDDD.Domain.Entities;
 
 namespace GestaoDDD.Infra.Data.Contexto
 {
-   public interface IGestaoContext
+    public interface IGestaoContext
     {
         DbEntityEntry Entry(object entity);
         int SaveChanges();
         DbSet<T> Set<T>() where T : class;
 
         DbSet<Categoria> Categoria { get; set; }
+        DbSet<Prestador> Prestador { get; set; }
+        DbSet<Usuario> Usuario { get; set; }
+        DbSet<Orcamento> Orcamento { get; set; }
+        DbSet<Servico> Servico { get; set; }
+        DbSet<ServicoPrestador> ServicoPrestador { get; set; }
     }
 }
