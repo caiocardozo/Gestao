d32b43af69b580b647ed528822d2054220a29d1c
Wagner Nogueira <wagnogueira19@hotmail.com>
2015-12-29 18:47:46 -0200

    Identity pronto. Executa update-database para atualizar base

---
+++ GestaoDDD.Infra.Data/EntityConfig/ClientConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using GestaoDDD.Domain.Entities.Identity;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestaoDDD.Infra.Data.EntityConfig
+{
+    public class ClientConfiguration : EntityTypeConfiguration<Client>
+    {
+        public ClientConfiguration()
+        {
+            HasKey(u => u.Id);
+
+            Property(u => u.Id)
+                .IsRequired();
+
+            Property(u => u.ClientKey);
+
+            ToTable("AspNetClients");
+
+        }
+    }
+}
