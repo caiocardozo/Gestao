dbd0b0a99bd178606e719e0303ca1bab199b38a8
Wagner Nogueira <wagnogueira19@hotmail.com>
2016-02-23 18:39:35 -0300

    repositoryComFUnc

---
+++ GestaoDDD.Infra.Data/Repositories/ComoFuncionaRepository.cs
@@ -0,0 +1,17 @@
+﻿using GestaoDDD.Domain.Entities;
+using GestaoDDD.Domain.Interfaces.Repositories;
+using GestaoDDD.Infra.Data.Contexto;
+
+namespace GestaoDDD.Infra.Data.Repositories
+{
+    public class ComoFuncionaRepository : RepositoryBase<ComoFunciona>, IComoFuncionaRepository
+    {
+        private readonly GestaoContext _db;
+        public ComoFuncionaRepository(GestaoContext gestaoContexto)
+            : base(gestaoContexto)
+        {
+            _db = new GestaoContext();
+
+        }
+    }
+}
