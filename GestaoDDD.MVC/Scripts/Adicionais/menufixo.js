c48b5eb472cbea5d1137ad2018880f83afcfd80c
Paulo Henrique Freitas <paulo-henriquee@hotmail.com>
2016-03-02 01:44:50 -0300

    View Home Finalizada - Front-End

---
+++ GestaoDDD.MVC/Scripts/Adicionais/menufixo.js
@@ -0,0 +1,11 @@
+﻿
+      $(function(){
+          var div = $('#menuHeader');
+          $(window).scroll(function () {
+              if ($(this).scrollTop() > 50) {
+                  div.addClass("menu-fixo");
+              } else {
+                  div.removeClass("menu-fixo");
+              }
+          });
+      });
