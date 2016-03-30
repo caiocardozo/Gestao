63d6d41c544a051087d584b64ee2b3e145499642
Paulo Henrique Freitas <paulo-henriquee@hotmail.com>
2016-03-29 23:09:36 -0300

    Front-End - Quem Somos, Termos de Uso e Cadastro Prestador + Links rodapé

---
+++ GestaoDDD.MVC/Controllers/QuemSomosController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GestaoDDD.MVC.Controllers
+{
+    public class QuemSomosController : Controller
+    {
+        // GET: QuemSomos
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ GestaoDDD.MVC/Controllers/TermosDeUsoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GestaoDDD.MVC.Controllers
+{
+    public class TermosDeUsoController : Controller
+    {
+        // GET: TermosDeUso
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
