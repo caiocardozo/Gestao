8cb3d4dfc74ae5620126e435448ddc555c4585de
Paulo Henrique Freitas <paulo-henriquee@hotmail.com>
2016-04-04 23:14:25 -0300

    Routes

--- GestaoDDD.MVC/App_Start/RouteConfig.cs
+++ GestaoDDD.MVC/App_Start/RouteConfig.cs
@@ -9,11 +9,27 @@ namespace GestaoDDD.MVC
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+
+            routes.MapRoute(
+            name: "Contato",
+            url: "Contato",
+            defaults: new
+            {
+                controller = "Contato",
+                action = "Create",
+                id = UrlParameter.Optional
+            });
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+
+
+            
+
+           
 
             //a rota tava Prestador, create.
         }
